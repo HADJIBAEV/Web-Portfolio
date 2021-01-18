e90654d6b21326d3c45a672f9f1333a63a97eaca
Kahramon <kahramonhadjibaev@gmail.com>
2021-01-19 00:41:03 +0300

    Created

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, render_template, redirect, url_for, jsonify
+
+app = Flask(__name__, static_folder="static")
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(f'{page_name}.html')
+
+# def write_to_file(data):
+#     with open('./database.txt', mode='a') as database:
+#         email = data["email"]
+#         subject = data["subject"]
+#         message = data["message"]
+#         file = database.write(f'\n{email},{subject},{message}')
+#
+#
+# def write_to_csv(data):
+#     with open('./database.csv', mode='a', newline='') as database2:
+#         email = data["email"]
+#         subject = data["subject"]
+#         message = data["message"]
+#         csv_writer = csv.writer(database2, delimiter=',',
+#                                 quotechar='"', quoting=csv.QUOTE_MINIMAL)
+#         csv_writer.writerow([email, subject, message])
+#
+#
+# @app.route('/submit_form', methods=['POST', 'GET'])
+# def submit_form():
+#     if request.method == 'POST':
+#         try:
+#             data = request.form.to_dict()
+#             write_to_csv(data)
+#             return redirect('/Thankyou')
+#         except:
+#             return 'Did not save to database'
+#     else:
+#         return 'Something went wrong!'
+#
+#
+# if __name__ == '__main__':
+#     # Threaded option to enable multiple instances for multiple user access support
+#     app.run(threaded=True, port=5000)
---
+++ static/script.js
@@ -0,0 +1,6 @@
+// document.addEventListener('DOMContentLoaded', function (){
+// 	particlesJS.load('particles-js','particles.json');
+// 	})
+// const server = require('http').createServer();
+// const port = process.env.PORT || 3000;
+// server.listen(port, () => console.log(`Listening on ${port}`));
